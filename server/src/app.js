f2558b579570b51a44cc5c706871a93987edca5e
Jagdish Padhi <jagdishpadhi9@gmail.com>
2026-05-10 15:37:58 +0530

    feat: fix deployed issue

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: true,
   credentials: true,
 }));
 app.use(cookieParser());
